3ce29765010da6ecd931398630be9286338dd328
Scott Bruton <scottpbruton@gmail.com>
2025-01-22 11:15:15 +1100

    updater powershell working and starting app

--- test-powershell.js
+++ test-powershell.js
@@ -1,41 +1,49 @@
-const { spawn } = require('child_process');
-const path = require('path');
+const { spawn } = require("child_process");
+const path = require("path");
 
 // Path to the PowerShell script
-const psScriptPath = "C:\\Users\\bruto\\AppData\\Local\\Temp\\UpdateScript.ps1";
-const installerPath = "C:\\Users\\bruto\\AppData\\Local\\Temp\\PrintPhotoApp-Setup.exe";
+const psScriptPath = "C:\\Programming\\PrintPhotoApp\\UpdateScript.ps1";
+const installerPath =
+  "C:\\Users\\bruto\\AppData\\Local\\Temp\\PrintPhotoApp-Setup.exe";
 
-console.log('Starting PowerShell test...');
-console.log('Script path:', psScriptPath);
-console.log('Installer path:', installerPath);
+console.log("Starting PowerShell test...");
+console.log("Script path:", psScriptPath);
+console.log("Installer path:", installerPath);
 
 // Create PowerShell process
-const ps = spawn('powershell.exe', [
-    '-NoProfile',
-    '-ExecutionPolicy', 'Bypass',
-    '-File', psScriptPath,
-    '-InstallerPath', installerPath
-], {
-    stdio: 'pipe',
-    windowsHide: false
-});
+const ps = spawn(
+  "powershell.exe",
+  [
+    "-NoProfile",
+    "-ExecutionPolicy",
+    "Bypass",
+    "-File",
+    psScriptPath,
+    "-InstallerPath",
+    installerPath,
+  ],
+  {
+    stdio: "pipe",
+    windowsHide: false,
+  }
+);
 
 // Handle PowerShell output
-ps.stdout.on('data', (data) => {
-    console.log('PowerShell output:', data.toString());
+ps.stdout.on("data", (data) => {
+  console.log("PowerShell output:", data.toString());
 });
 
 // Handle PowerShell errors
-ps.stderr.on('data', (data) => {
-    console.error('PowerShell error:', data.toString());
+ps.stderr.on("data", (data) => {
+  console.error("PowerShell error:", data.toString());
 });
 
 // Handle process exit
-ps.on('close', (code) => {
-    console.log('PowerShell process exited with code:', code);
+ps.on("close", (code) => {
+  console.log("PowerShell process exited with code:", code);
 });
 
 // Handle process errors
-ps.on('error', (err) => {
-    console.error('Failed to start PowerShell:', err);
-}); 
+ps.on("error", (err) => {
+  console.error("Failed to start PowerShell:", err);
+});
